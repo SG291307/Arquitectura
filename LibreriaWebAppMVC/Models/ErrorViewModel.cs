a93eb60dd94f10addfc5e798a62e33ca5e365025
admfacs-garcia <admfacs-garcia@FACS-ASI-03>
2026-08-21 16:36:33 -0300

    Agregar archivos de proyecto.

---
+++ LibreriaWebAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibreriaWebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
