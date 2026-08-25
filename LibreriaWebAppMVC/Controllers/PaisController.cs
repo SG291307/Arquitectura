114a49779f1a2fd8fe040f67b48e3114fd92ee1a
SG291307 <SG291307@fi365.ort.edu.uy>
2026-08-24 21:53:00 -0300

    Se crea controlador pais y vista guardar pais.

---
+++ LibreriaWebAppMVC/Controllers/PaisController.cs
@@ -0,0 +1,31 @@
+﻿using Libreria.LogicaAplicaion.CasosUso.CUPais;
+using Libreria.LogicaNegocio.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibreriaWebAppMVC.Controllers
+{
+    public class PaisController : Controller
+    {
+        private CUAltaPais cUAltaPais = new CUAltaPais();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public IActionResult Create(Pais p)
+        {
+            cUAltaPais.Ejecutar(p);
+            return View();
+        }
+    }
+}
